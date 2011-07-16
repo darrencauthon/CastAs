c01caf40a535c4194c897a7b4e7bb114d0221697
Darren Cauthon <darren@cauthon.com>
2011-07-16 18:55:13 -0500

    Inital commit.

---
+++ CastAs.Tests/CastAsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using Should;
+
+namespace CastAs.Tests
+{
+    [TestFixture]
+    public class CastAsTests
+    {
+        [Test]
+        public void Returns_the_object_passed_in()
+        {
+            var fruit = new Fruit();
+            fruit.CastAs<Fruit>().ShouldBeSameAs(fruit);
+
+            var apple = new Apple();
+            apple.CastAs<Fruit>().ShouldBeSameAs(apple);
+        }
+    }
+
+    public class Fruit
+    {
+    }
+
+    public class Apple : Fruit
+    {
+    }
+}
---
+++ CastAs/CastingHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    public static class CastingHelpers
+    {
+        public static T CastAs<T>(this object @object)
+        {
+            return (T) @object;
+        }
+    }
+}
